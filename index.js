047d7c01c17086acd4bf991cb426d7eb54f6de5b
anmiansu <anmiansu@gmail.com>
2025-04-01 17:37:32 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// se guardan las funciones de la libreria http
+const http = require("http");
+
+//controla las peticiones que llegan al servidor
+function requestController() {
+    console.log("Recibimos una nueva petición");
+}
+
+//configurar nuestro servidor
+const server = http.createServer(requestController);
+
+server.listen(4000)
